4f23ed7e843a3cc8fec65908e42d20be4056a6b9
Alexander Sagalovskiy <spikesagal@gmail.com>
2024-06-25 15:39:19 -0600

    error state

--- src/app/components/npm-search/index.js
+++ src/app/components/npm-search/index.js
@@ -4,16 +4,28 @@ import { useState, useEffect } from 'react';
 import { useDebounce } from 'use-debounce';
 import styles from './styles.module.css';
 
+const ERROR_MESSAGE = 'We\'re having trouble connecting to npm.';
+
 export default function NpmSearchComponent() {
   const [queryText, setQueryText] = useState('');
   const [debouncedQueryText] = useDebounce(queryText, .5 * 1000);
   const [results, setResults] = useState([]);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchModules = async () => {
       console.log(debouncedQueryText);
-      const resp =  await fetch(`https://api.npms.io/v2/search/suggestions?q=${debouncedQueryText}`);
-      setResults(await resp.json());
+      try {
+        const resp = await fetch(`https://api.npms.io/v2/search/suggestions?q=${debouncedQueryText}`);
+        if (resp.status === 200) {
+          setError(null);
+          setResults(await resp.json());
+        } else {
+          setError(ERROR_MESSAGE);
+        }
+      } catch (e) {
+        setError(ERROR_MESSAGE);
+      }
     }
 
     if (debouncedQueryText) {
@@ -31,8 +43,11 @@ export default function NpmSearchComponent() {
         onChange={(evt) => setQueryText(evt.target.value)}
       />
       <ul className={styles.Results}>
+        {error && (
+          <li className={styles.Error}>{error}</li>
+        )}
         {results.map(result => {
-          const {name, description, version, links: {npm}} = result.package;
+          const {name = '', description = '', version = '', links: {npm = ''} = {npm: ''}} = result.package;
           return (
             <li>
               <a href={npm}>
